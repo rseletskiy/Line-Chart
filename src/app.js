16765b9226ba7c1b36df1d38a1ac2ef9ad5e90d2
rseletskiy <seletskiyweb@gmail.com>
2022-12-08 15:03:01 +0200

    AAdd data.json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import { chart } from "./chart";
-import { getChartData } from "./data";
+import data from './data.json';
 import "./styles.scss";
 
-const lineChart = chart(document.getElementById("chart"), getChartData());
-lineChart.init();
+const lineChart1 = chart(document.getElementById("chart-1"), data[0]);
+lineChart1.init();
+
+const lineChart2 = chart(document.getElementById("chart-2"), data[4]);
+lineChart2.init();
